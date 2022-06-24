5c92fe092b5d2716cd4f3a57039a5abb594d2726
unknown <janhavidahatonde@gmail.com>
2022-06-24 14:31:27 +0530

    This(Keyword)

---
+++ src/ThisKeyword/Car.java
@@ -0,0 +1,29 @@
+package ThisKeyword;
+
+public class Car {
+    int carNo ;
+    String carName;
+    double Price;
+
+
+
+    public Car(int carNo, String carName, double price) {
+        this.carNo = carNo;
+        this.carName = carName;
+        Price = price;
+    }
+
+    void display(){
+        System.out.println("***************Car Details********************");
+        System.out.println("car no: "+ carNo);
+        System.out.println("car name: "+carName);
+        System.out.println("Price: "+Price);
+    }
+}
+
+class CarImpl{
+    public static void main(String[] args) {
+        Car c1 = new Car(4845, "Jaguar", 8000000);
+        c1.display();
+    }
+}
