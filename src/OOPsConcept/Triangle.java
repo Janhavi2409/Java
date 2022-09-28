cd7cdad32afdbd6ab99d5277c0836b5b1e9ab442
unknown <janhavidahatonde@gmail.com>
2022-09-28 17:32:49 +0530

    OOPs Concept

--- src/OOPsConcept/Triangle.java
+++ src/OOPsConcept/Triangle.java
@@ -1,6 +1,6 @@
 package OOPsConcept;
 
-class Tri{
+class Tri {
     int base;
     int height;
 
@@ -20,20 +20,21 @@ class Tri{
         this.height = height;
     }
 
-    public void area(){
-        double area = 0.5*base*height;
-        System.out.println("Area = "+area+" units");
+    public void area() {
+        double area = 0.5 * base * height;
+        System.out.println("Area = " + area + " units");
     }
 
 }
+
 public class Triangle {
     public static void main(String[] args) {
         System.out.println("TRIANGLE");
         Tri t1 = new Tri();
         t1.setBase(8);
         t1.setHeight(4);
-        System.out.println("Base = "+t1.getBase()+" units");
-        System.out.println("Height = "+t1.getHeight()+" units");
+        System.out.println("Base = " + t1.getBase() + " units");
+        System.out.println("Height = " + t1.getHeight() + " units");
         t1.area();
 
     }
